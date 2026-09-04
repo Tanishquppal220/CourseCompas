fb74021662d4a97fb1a6da2384e7afee5a874710
Tanishq Uppal <tanishquppal220@gmail.com>
2026-09-04 20:49:58 +0530

    Cleaned the Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Landing } from "./pages/Landing";
-import { ChatPage } from "./pages/ChatPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/chat" element={<ChatPage />} />
-      </Routes>
-    </BrowserRouter>
+   <>
+   Hello World
+   </>
   );
 }
 
